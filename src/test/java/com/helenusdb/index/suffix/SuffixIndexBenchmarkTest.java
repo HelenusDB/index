9974c70181b400d77a1e1a0114c320bf68998a10
toddfredrich <toddfredrich@localhost>
2025-08-13 16:58:42 -0700

    Organized imports

--- src/test/java/com/helenusdb/index/suffix/SuffixIndexBenchmarkTest.java
+++ src/test/java/com/helenusdb/index/suffix/SuffixIndexBenchmarkTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 
 import com.helenusdb.index.Corpus;
-import com.helenusdb.index.suffix.SuffixIndex;
 
 class SuffixIndexBenchmarkTest
 {
